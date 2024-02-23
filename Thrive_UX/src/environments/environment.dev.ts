5a1f5399c0ddb982fa843339f63abe969112a181
Kephotho Solutions <30622305+kephothoX@users.noreply.github.com>
2024-02-23 19:47:33 +0300

    Update environment.dev.ts

--- Thrive_UX/src/environments/environment.dev.ts
+++ Thrive_UX/src/environments/environment.dev.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  B2C_PROJ_ID:  'P2bzm8ZYJ0fD8n6aEHbGiXS4bVjL',
+  B2C_PROJ_ID:  '',
   BASE_URL: 'https://api.descope.com',
   ENDPOINT: '/api',
   USER_FLOW_ID: 'sign-up-or-in',
-  B2B_PROJ_ID: 'P2cC4YzygthpPTF11mXVnJVK3PnW'
+  B2B_PROJ_ID: ''
 };
